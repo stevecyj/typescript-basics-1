b9510062a7e5253787a4aa591e2d4fa68b630db2
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-01-11 17:40:52 +0800

    feat: add CreateRoom class with properties and methods
    - Add a new class `CreateRoom` with properties and methods
    - Initialize `room` property with a value from `name` variable
    - Add a method `addFamilyMember` to add names to the `family` array
    - Add a method `showFamily` to log the `family` array
    - Add a method `cleanRoom` to log a cleaning message
    - Create instances of `CreateRoom` for `roomJohn` and `roomJane`
    - Access the `family` property of `roomJohn`
    - Add a family member to `roomJohn`
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- src/main.ts
+++ src/main.ts
@@ -18,3 +18,29 @@ console.log(
 type PersonNewName = string
 const person6: PersonNewName = "John"
 console.log(`New Developer is ${person6}`)
+
+class CreateRoom {
+  public room: string
+  private family: string[] = []
+  constructor(room: string) {
+    this.room = `${name}'s room`
+  }
+
+  addFamilyMember(name: string) {
+    this.family.push(name)
+  }
+
+  showFamily() {
+    console.log(this.family)
+  }
+
+  cleanRoom(soap: string) {
+    console.log(`Cleaning ${this.room} with ${soap}`)
+  }
+}
+
+const roomJohn = new CreateRoom("John")
+const roomJane = new CreateRoom("Jane")
+
+roomJohn.family
+roomJohn.addFamilyMember("John")
