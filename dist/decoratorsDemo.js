f2ee2619a03d3d0bca073bca32fbc08ad78c8f58
stevecyj <ycsteven.tsao@gmail.com>
2024-01-13 09:43:26 +0800

    feat: refactor codebase to improve code organization and remove unused functions
    - Remove the `Helper` function declaration and its usage
    - Add the `__param` function declaration and its usage
    - Remove the `console.log` statements inside the `Helper` function
    - Add the `Employee` class declaration
    - Add the `Log` property decorator
    - Add the `Log2` accessor decorator
    - Add the `Log3` method decorator
    - Add the `Log4` parameter decorator
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- dist/decoratorsDemo.js
+++ dist/decoratorsDemo.js
@@ -5,12 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Helper(genericString) {
-    return function (constructor) {
-        console.log(genericString);
-        console.log(constructor);
-    };
-}
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 let Car = class Car {
     constructor() {
         this.name = "Bugatti Veyron";
@@ -23,6 +20,12 @@ Car = __decorate([
 ], Car);
 const car1 = new Car();
 console.log(car1);
+function Helper(genericString) {
+    return function (constructor) {
+        console.log(genericString);
+        console.log(constructor);
+    };
+}
 function AngularTemplate(template, hookId) {
     return function (_) {
         const hookEl = document.getElementById(hookId);
@@ -30,4 +33,51 @@ function AngularTemplate(template, hookId) {
             hookEl.innerHTML = template;
         }
     };
+}
+class Employee {
+    set fullName(name) {
+        this._fullName = name;
+    }
+    constructor(title, name) {
+        this.title = title;
+        this._fullName = name;
+    }
+    getNameWithTitle(id) {
+        return `Employee - ${id}, Title - ${this.title}, Name - ${this._fullName}`;
+    }
+}
+__decorate([
+    Log
+], Employee.prototype, "title", void 0);
+__decorate([
+    Log2
+], Employee.prototype, "fullName", null);
+__decorate([
+    Log3,
+    __param(0, Log4)
+], Employee.prototype, "getNameWithTitle", null);
+// property decorators
+function Log(target, propertyName) {
+    console.log("Property decorator");
+    console.log(target, propertyName);
+}
+function Log2(target, name, descriptor) {
+    console.log("Accessor decorator");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+// method decorators
+function Log3(target, name, descriptor) {
+    console.log("Method decorator");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+// parameter decorators
+function Log4(target, name, position) {
+    console.log("Parameter decorator");
+    console.log(target);
+    console.log(name);
+    console.log(position);
 }
--- src/decoratorsDemo.ts
+++ src/decoratorsDemo.ts
@@ -1,10 +1,3 @@
-function Helper(genericString: string) {
-  return function (constructor: Function) {
-    console.log(genericString)
-    console.log(constructor)
-  }
-}
-
 @Helper("Showing constructor:")
 @AngularTemplate("<h1>My Angular App</h1>", "app")
 class Car {
@@ -17,6 +10,13 @@ class Car {
 const car1 = new Car()
 console.log(car1)
 
+function Helper(genericString: string) {
+  return function (constructor: Function) {
+    console.log(genericString)
+    console.log(constructor)
+  }
+}
+
 function AngularTemplate(template: string, hookId: string) {
   return function (_: any) {
     const hookEl = document.getElementById(hookId)
@@ -24,4 +24,58 @@ function AngularTemplate(template: string, hookId: string) {
       hookEl.innerHTML = template
     }
   }
+}
+
+class Employee {
+  @Log
+  title: string
+  private _fullName: string
+
+  @Log2
+  set fullName(name: string) {
+    this._fullName = name
+  }
+
+  constructor(title: string, name: string) {
+    this.title = title
+    this._fullName = name
+  }
+
+  @Log3
+  getNameWithTitle(@Log4 id: number) {
+    return `Employee - ${id}, Title - ${this.title}, Name - ${this._fullName}`
+  }
+}
+
+// property decorators
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator")
+  console.log(target, propertyName)
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor decorator")
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+// method decorators
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator")
+  console.log(target)
+  console.log(name)
+  console.log(descriptor)
+}
+
+// parameter decorators
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter decorator")
+  console.log(target)
+  console.log(name)
+  console.log(position)
 }
